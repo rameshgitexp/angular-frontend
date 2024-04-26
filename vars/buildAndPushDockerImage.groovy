78de6151831aa950c13bbcf73ae805fa5df72628
rameshgitexp <141246903+rameshgitexp@users.noreply.github.com>
2024-04-26 09:01:20 +0530

    Update buildAndPushDockerImage.groovy

--- vars/buildAndPushDockerImage.groovy
+++ vars/buildAndPushDockerImage.groovy
@@ -36,7 +36,7 @@ def call(String imageName, String awsAccountId, String awsRegion) {
                 steps {
                     script {
                         bat "docker pull ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:ramesh"
-                        bat "docker run -d -p 4200:4220 ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:ramesh"
+                        bat "docker run -d -p 4000:4200 ${awsAccountId}.dkr.ecr.${awsRegion}.amazonaws.com/${imageName}:ramesh"
                         bat 'docker ps'
                         bat 'docker ps -a'
                     }
